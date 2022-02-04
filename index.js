7aff8b39f8bdc03dad754ce0d36a082f7dfda22b
enti <enti@enti.enti>
2022-02-04 10:30:00 +0100

    Pequeño servidor web

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+
+let http = require("http");
+
+http.createServer(function(req, res) {
+	res.writeHead(200); 
+
+	let saludo = "<h1>ola k ase</h1>";
+
+	res.end(saludo);
+
+}
+).listen(3000);
