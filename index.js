b47af17b62fa63c60ebd6fcb49da7db54bd86445
enti <enti@enti.enti>
2022-05-14 23:55:30 +0200

    Entrega final

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let http = require("http");
 
 let mongo_client = require("mongodb").MongoClient;
+let ObjectId = require("mongodb").ObjectID;
 
 let url = "mongodb://localhost";
 
@@ -10,6 +11,7 @@ let db;
 let fs = require("fs");
 
 console.log("iniciando servidor");
+
 mongo_client.connect(url, function(err, conn ){
 
 	console.log("Dentro de Mongo");
@@ -21,6 +23,30 @@ mongo_client.connect(url, function(err, conn ){
 	
 	db = conn.db("tffhd");
 });
+
+
+function create_list(res, ret_value){
+
+	let col;
+
+	if(ret_value == "characters" ){
+		col = "characters"
+	}else if(ret_value = "items"){
+	col = "items";}
+	else{ res.end();return;}
+
+	let collection_used = db.collection(col).find();
+	let col_arr = [];
+	let char_string ;
+
+
+	collection_used.toArray(function(err,data){
+			char_string = JSON.stringify(data);
+			res.end(char_string);
+			console.log ("list ready")
+			})
+	};
+
 
 http.createServer(function(req, res) {
  res.writeHead(200);
@@ -35,61 +61,87 @@ http.createServer(function(req, res) {
 		return;
 	}
 
-	if (req.url == "/characters") {
-		
+	let split_url  = req.url.split("/");
+	
+	if (split_url.length == 2) {		
 
-		ret_val = "characters";
+		create_list(res, split_url[1]);
+		return;
 
 	
-	}else if (req.url == "/items"){
+	} if(split_url[2].length != 24){
+		res.end()
+		return;
+		}
+	
+	if (split_url[1] == "characters"){
+		let chara_id = new ObjectId(split_url[2]);
+		let chara_data = db.collection("characters").find({"_id":chara_id});
 
-		ret_val = "items";
+		chara_data.toArray(function(err,data){ 
+			let data_string = JSON.stringify(data);
+			
+			res.end(data_string);
+			});
 
+			return;
+	}	
+	if(split_url[1] == "removeChara"){
+		let rChara_id = new ObjectId(split_url[2]);
+
+		let rChara_data = db.collection("characters").find({"_id":rChara_id});
+		let data_string; 
+
+		db.collection("characters").deleteOne({"_id":rChara_id});
+		rChara_data.toArray(function(err,data){
+	
 		
-	}else if (req.url == "/weapons"){
+		let resEnd_string = "";
+		if (data != "" ){	
+		resEnd_string = "<H1> Personaje " + data[0].name  + " eliminado! </H1>"; }
+		else {
+		resEnd_string = "No existe!";
+		}
+		res.end(resEnd_string);
+				
+		});
+		return;
+}
+	if (split_url[1] == "items"){
+		let item_id = new ObjectId(split_url[2]);
+		let item_data = db.collection("items").find({"_id":item_id});
 
-		ret_val = "weapons";
-	}else
+		item_data.toArray(function(err,data){ 
+			let data_string = JSON.stringify(data);
+			
+			res.end(data_string);
+			});
+			return;
+	}	
+	if(split_url[1] == "removeItem"){
+		let rItem_id = new ObjectId(split_url[2]);
+
+		let rItem_data = db.collection("items").find({"_id":rItem_id});
+		let data_string;
+		
+		db.collection("items").deleteOne({"_id":rItem_id});
+		rItem_data.toArray(function(err,data){
+		let resEnd_string = "";
+		if ( data !=""){
+		resEnd_string = "<H1> Objeto " + data[0].item  + " eliminado! </H1>"; 
+
+		}else{resEnd_string = "No hay nada!"}
+		res.end(resEnd_string);
+		
+		});
+	return;
+	}
+	else
 	{	
 		res.end();	
 		return;
 	}
-	
-	let split_url = req.url.split("/");
-	
-
-
-	let collection_used = db.collection(ret_val).find();
-	let col_arr = [];
-	let char_string ;
-
-
-	collection_used.toArray(function(err,data){
-	
-	char_string = JSON.stringify(data);
-
-	res.end(char_string);
-	});
-
-
-
-;
-
-
-/*	characters.forEach(function(character){chara_arr.push(character);
-
-	
-	console.log(chara_arr);
-
-	}
-
-	);*/
-	
-
-
-
-}
-).listen(3000);
+	}).listen(3000);
 
 
 
